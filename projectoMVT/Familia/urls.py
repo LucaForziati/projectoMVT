84a5b0ce7a671c8d342a4990191b6873838f8597
Luca Forziati Gangi <lucaforziati@hotmail.com>
2022-03-29 14:00:29 -0300

    agregue la opcion de agregar un familiar a la base de datos

--- projectoMVT/Familia/urls.py
+++ projectoMVT/Familia/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from Familia import views
 
 urlpatterns = [
-    path("", views.familia)
+    path("", views.familia),
+    path("agregar-familiar/<nombre>/<apellido>/<num_de_la_suerte>/<nacimiento>", views.agregarFamiliar)
 ]
--- projectoMVT/Familia/views.py
+++ projectoMVT/Familia/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.template import loader
+from django.template import Context, loader
+
+from Familia.models import Familia
 
 # Create your views here.
 
@@ -8,3 +10,22 @@ def familia(request):
 
     plantilla = loader.get_template("familia.html")
     return HttpResponse(plantilla.render())
+
+def agregarFamiliar(request, nombre, apellido, num_de_la_suerte, nacimiento):
+
+    familiar_nuevo = Familia(
+        nombre = nombre, 
+        apellido = apellido,
+        num_de_la_suerte = num_de_la_suerte,
+        nacimiento = nacimiento
+        )
+    familiar_nuevo.save()
+
+    plantilla = loader.get_template("agregarFamiliar.html")
+    return HttpResponse(plantilla.render({
+        "nombre": familiar_nuevo.nombre, 
+        "apellido": familiar_nuevo.apellido, 
+        "num_de_la_suerte": familiar_nuevo.num_de_la_suerte, 
+        "nacimiento": familiar_nuevo.nacimiento
+        }))
+    
