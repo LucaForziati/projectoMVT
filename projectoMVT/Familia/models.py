b032664a21ef501dc2d30d94d72dffe5081b2bf6
Luca Forziati Gangi <lucaforziati@hotmail.com>
2022-03-29 12:51:19 -0300

    agregue el modelo Familia

--- projectoMVT/Familia/models.py
+++ projectoMVT/Familia/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Familia(models.Model):
+
+    nombre = models.CharField("nombre", max_length = 50)
+    apellido = models.CharField("apellido", max_length = 50)
+    num_de_la_suerte = models.IntegerField("num_de_la_suerte")
+    nacimiento = models.DateField("nacimiento", max_length = 50)
